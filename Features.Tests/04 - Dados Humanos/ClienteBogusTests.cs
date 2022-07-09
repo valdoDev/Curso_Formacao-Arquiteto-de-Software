683af8344ac108da40f5d39dbd27ce30a46f1eda
valdoDev <valdo.developer@gmail.com>
2022-07-09 16:22:06 -0300

    Add exemplo Bogus

---
+++ Features.Tests/04 - Dados Humanos/ClienteBogusTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteBogusTests
+    {
+        private readonly ClienteTestsBogusFixture _clienteTestsBogusFixture;
+
+        public ClienteBogusTests(ClienteTestsBogusFixture clienteTestsBogusFixture)
+        {
+            _clienteTestsBogusFixture = clienteTestsBogusFixture;
+        }
+
+        [Fact(DisplayName ="Novo Cliente Valido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = _clienteTestsBogusFixture.GerarClienteValido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+
+
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = _clienteTestsBogusFixture.GerarClienteInvalido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
