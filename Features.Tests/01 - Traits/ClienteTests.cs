039366b424969261dac752a4eb520f4185ecd9af
valdoDev <valdo.developer@gmail.com>
2022-07-09 14:13:02 -0300

    Criado projeto e adicionado exmeplo de Traits e Fixtures

---
+++ Features.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,56 @@
+﻿using Features.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo ClienteValido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Eduardo",
+                "Pires",
+                DateTime.Now.AddYears(-30),
+                "edo@edu.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+
+        }
+
+        [Fact(DisplayName = "Novo ClienteInvalido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now.AddYears(-2),
+                "edo2edu.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
