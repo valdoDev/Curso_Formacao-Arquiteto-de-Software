e76032a297cd8bf5c67fc318105f0516ae8be288
valdobatista <valdobatista@VALDOBATISTF288>
2022-08-25 10:27:58 -0300

    Finalizado com base na aula 7.10

---
+++ 2_Modelagem_Dominios_Ricos/src/NerdStore.Catalogo.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ProdutoViewModel, Produto>()
+                .ConstructUsing(vm =>
+                    new Produto(vm.Nome, vm.Descricao, vm.Ativo,
+                        vm.Valor, vm.CategoriaId, vm.DataCadastro,
+                        vm.Imagem, new Dimensoes(vm.Altura, vm.Largura, vm.Profundidade)));
+
+            CreateMap<CategoriaViewModel, Categoria>()
+                .ConstructUsing(vm => new Categoria(vm.Nome, vm.Codigo));
+        }
+    }
+}
